289c832ae0edaac8c3a9e03678df9dfd65d2493b
ZerataX <mail@zera.tax>
2020-02-10 16:20:07 +0100

    fix ocal comment out yacx

--- src/sumarray.cpp
+++ src/sumarray.cpp
@@ -4,66 +4,92 @@
 #include <string>
 #include <vector>
 
-#include "../lib/ocal/ocal.hpp"
-#include "../lib/yacx/main.hpp"
+#include "../lib/ocal/ocal_cuda.hpp"
+// #include "../lib/yacx/include/yacx/main.hpp"
 
 const std::string cudasource =
-    "extern C global void sumArrayOnGPU (\n" +
-    "float *A , float *B , float * C , int * size ) {\n" +
-    "int i_inBlock = threadIdx . x + threadIdx . y * blockDim . x + threadIdx "
-    ". z * blockDim . y * blockDim . x;\n" +
-    "int blockID = blockIdx . x;\n" +
-    "int i = i_inBlock + blockID *( blockDim . x * blockDim . y * blockDim . z "
-    ");\n" +
-    "if (i <=* size ) {\n" + "C [ i ]= A [ i ]+ B [ i ];\n" + "}\n" + "}";
-const size_t nElement = 1024;
+    "extern C global void sumArrayOnGPU (float *A , float *B , float * C , int "
+    "* size ) {\n"
+    "   int i_inBlock = threadIdx.x + threadIdx.y * blockDim.x + threadIdx.z * "
+    "blockDim.y * blockDim.x;\n"
+    "   int blockID = blockIdx . x;\n"
+    "   int i = i_inBlock + blockID *( blockDim.x * blockDim.y * blockDim.z);\n"
+    "   if (i <=* size )\n"
+    "      C [ i ]= A [ i ]+ B [ i ];\n"
+    "}";
+const size_t nElements = 1024;
 const size_t nBytes = nElements * sizeof(float);
 
-void yacx() {
-  Device dev;
-  Source source{cudasource};
-  std::vector<float> inA, inB, out;
-  inA.resize(SIZE);
-  inB.resize(SIZE);
-  out.resize(out);
+struct RandomGenerator {
+  int maxValue;
+  RandomGenerator(int max) : maxValue(max) {}
 
-  std::generate(inA.begin(), inA.end(), std::rand());
-  std::generate(inB.begin(), inA.end(), std::rand());
+  float operator()() { return static_cast<float>(rand() % maxValue); }
+};
 
-  std::vector<KernelArg> args;
-  args.emplace_back(KernelArg{inA.data(), nBytes, false});
-  args.emplace_back(KernelArg{inB.data(), nBytes, false});
-  args.emplace_back(KernelArg{out.data(), nBytes, true});
-  args.emplace_back(KernelArg{nElements});
+// void yacxSumarray() {
+//   Device dev;
+//   Source source{cudasource};
+//   std::vector<float> inA, inB, out;
+//   inA.resize(nElements);
+//   inB.resize(nElements);
+//   out.resize(out);
+//
+//   std::generate(inA.begin(), inA.end(), RandomGenerator(100));
+//   std::generate(inB.begin(), inA.end(), RandomGenerator(100));
+//
+//   std::vector<KernelArg> args;
+//   args.emplace_back(KernelArg{inA.data(), nBytes, false});
+//   args.emplace_back(KernelArg{inB.data(), nBytes, false});
+//   args.emplace_back(KernelArg{out.data(), nBytes, true});
+//   args.emplace_back(KernelArg{nElements});
+//
+//   Options options{yacx::options::GpuArchitecture(device),
+//                   yacx::options::FMAD(false)};
+//
+//   dim3 block(dev.max_block_dim);  // dim3 block(nElements);
+//   dim3 grid(1);
+//   source.program("sumArrayOnGPU")
+//         .compile(options)
+//         .configure(block, grid)
+//         .launch(args);
+//
+//   for (int i = 0; i < out.size(); i++) std::cout << out.at(i) << ' ';
+// }
 
-  dim3 block(dev.max_block_dim);  // dim3 block(nElement);
-  dim3 grid(1);
-  source.program("sumArrayOnGPU").compile().configure(block, grid).launch(args);
+void ocalSumarray() {
+  ocal::device<CUDA> device(0);
 
-  for (int i = 0; i < out.size(); i++) std::cout << out.at(i) << ' ';
-}
+  ocal::kernel sumarray = ocal::kernel(
+      cuda::source(cudasource),
+      std::vector<std::string>{"--gpu-architecture=35", "--fmad=false"});
 
-void ocal {
-  // auto device = ocal::get_device<CUDA>();
-  ocal::device<CUDA> device;
+  std::vector<float> inAdata, inBdata, outdata;
+  inAdata.resize(nElements);
+  inBdata.resize(nElements);
+  outdata.resize(nElements);
 
-  ocal::kernel sumarray = cuda::source(cudasource);
+  std::generate(inAdata.begin(), inAdata.end(), RandomGenerator(100));
+  std::generate(inBdata.begin(), inAdata.end(), RandomGenerator(100));
 
   ocal::buffer<float> inA(nBytes);
   ocal::buffer<float> inB(nBytes);
   ocal::buffer<float> out(nBytes);
+  auto inA_ptr = inA.get_host_memory_ptr();
+  auto inB_ptr = inB.get_host_memory_ptr();
+  auto out_ptr = inB.get_host_memory_ptr();
+  for (int i = 0; i < inAdata.size(); ++i) inA_ptr[i] = inAdata.at(i);
+  for (int i = 0; i < inBdata.size(); ++i) inA_ptr[i] = inBdata.at(i);
 
-  std::generate(inA.begin(), inA.end(), std::rand());
-  std::generate(inB.begin(), inB.end(), std::rand());
+  int max_block;
+  device.information(CU_DEVICE_ATTRIBUTE_MAX_BLOCK_DIM_X, &max_block);
 
-  dev(sumarray(dim3(nElement), dim3(1))(
-      read(inA.begin(), inB.end()), read(in.begin(), in.end()),
-      write(out.begin, out.end()), nElements));
-  for (int i = 0; i < out.size(); i++) std::cout << out.at(i) << ' ';
+  device(sumarray)(dim3(max_block), dim3(1))(read(inA), read(inB), write(out),
+                                             nElements);
+  for (int i = 0; i < out.size(); i++) std::cout << out_ptr[i] << ' ';
 }
 
 int main() {
-  ocal();
-  yacx();
+  ocalSumarray();
+  // yacxSumarray();
 }
-
